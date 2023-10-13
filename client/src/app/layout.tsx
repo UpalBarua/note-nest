3d2d2558684dce94efc12f559e760607164bc097
upalbarua <upalbarua52@gmail.com>
2023-10-13 08:49:07 +0600

    initial commit

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Note Nest',
+  description:
+    'Your Personal Space for Notes and Ideas. Unleash your creativity, stay organized, and collaborate seamlessly with NoteNest. Elevate your productivity and creativity today.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ client/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+const Home = () => {
+  return (
+    <div className="text-2xl">
+      <h1>Note Nest</h1>
+      <Button>click me</Button>
+    </div>
+  );
+};
+
+export default Home;
